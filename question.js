6327dafd783e861fb94495a7a9c827dec4210e6e
Paushalidx8 <paushalidebnath15789@gmail.com>
2024-12-24 01:13:09 +0530

    first commit

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the full form of WHO?",
+        answer: "B. World Health Organisation.",
+        options: [
+            "A. Wild Health Organisation.",
+            "B. World Health Organisation.",
+            "C. Widest Horsing Organisation.",
+            "D. None of the above."
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is Mahatma Gandhi called?",
+        answer: "A. Father of nation.",
+        options: [
+            "A. Father of nation.",
+            "B. Mother of nation.",
+            "C. Father of india.",
+            "D. None of the above."
+        ]
+    },
+    {
+        numb: 3,
+        question: "Where is Gir National Park located?",
+        answer: "D. Gujarat.",
+        options: [
+            "A. Delhi.",
+            "B. West Bengal.",
+            "C. Both A & B.",
+            "D. Gujarat."
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which animal can live without drinking water for many days?",
+        answer: "A. Camel",
+        options: [
+            "A. Camel",
+            "B. Snake.",
+            "C. both A & B.",
+            "D. None of the above."
+        ]
+    },
+    {
+        numb: 5,
+        question: "How many UT's are there in India?",
+        answer: "B. 8",
+        options: [
+            "A. 4",
+            "B. 8",
+            "C. 1",
+            "D. 10"
+        ]
+    },
+];
